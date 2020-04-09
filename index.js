d953ce511663c10e55b92943a6470839c2fee1da
Andryteck <772316andrew@gmail.com>
2020-04-09 11:10:52 +0300

    feat: add reset button

--- index.js
+++ index.js
@@ -1,84 +1,100 @@
+let arr = [], ei, ej;
 
-let arr = [],
-    ei,
-    ej;
+let buttonReset = document.createElement('button')
+buttonReset.id = 'reset'
+document.body.appendChild(buttonReset)
+buttonReset.innerHTML = 'Размешать и начать'
 
-const newGame = () => {
+let buttonStop = document.createElement('button')
+buttonStop.id = 'reset'
+document.body.appendChild(buttonStop)
+buttonStop.innerHTML = 'Стоп'
 
-    for (let i = 0; i < 4; i++) {
-        arr[i] = [];
-        for (let j = 0; j < 4; j++) {
-            if (i + j != 6) {
-                arr[i][j] = i * 4 + j + 1;
-            }
-            else {
-                arr[i][j] = '';
-            }
-        }
-    }
-    let elemI = 3;
-    let elemJ = 3;
-    for (let i = 0; i < 2012; i++) {
-        switch (Math.random(3 * Math.random())) {
-            case 0: if (elemI != 0) swap(arr, elemI, elemJ, --elemI, elemJ); break;
-            case 1: if (ej != 3) swap(arr, elemI, elemJ, elemI, ++elemJ); break;
-            case 2: if (ei != 3) swap(arr, elemI, elemJ, ++elemI, elemJ); break;
-            case 3: if (ej != 0) swap(arr, elemI, elemJ, elemI, --elemJ);
-        }
-    }
-    let createLayout = document.createElement('div');
-    createLayout.classList.add('layout');
-    document.querySelector('body').appendChild(createLayout);
+let buttonSave = document.createElement('button')
+buttonSave.id = 'reset'
+document.body.appendChild(buttonSave)
+buttonSave.innerHTML = 'Сохранить'
 
-    for (let i = 0; i < 4; i++) {
-        let row = document.createElement('div');
-        row.classList.add('row');
-        document.querySelector('.layout').appendChild(row);
-        for (let j = 0; j < 4; j++) {
-            let numbers = document.createElement('div');
-            numbers.id = i + " " + j;
-            row.appendChild(numbers);
+let buttonResult = document.createElement('button')
+buttonResult.id = 'reset'
+document.body.appendChild(buttonResult)
+buttonResult.innerHTML = 'Результаты'
 
-            numbers.onclick = numbersClick;
-            numbers.innerHTML = arr[i][j];
+let box = document.createElement('div')
+box.id = 'box'
+document.body.appendChild(box)
 
-        }
-
-
-
-    }
-}
-newGame()
-
-const swapElement = (arr, [i1], [j1], [i2], [j2]) => {
-    let temp = arr[i1][j1];
+function swap(arr, i1, j1, i2, j2) {
+    t = arr[i1][j1];
     arr[i1][j1] = arr[i2][j2];
-    arr[i2][j2] = temp;
+    arr[i2][j2] = t;
 }
-
-const numbersClick = (e) => {
-    let el = event.target;
-
-    let i = el.id.charAt(0),
+window.onload = function () {
+    box = document.getElementById("box");
+    newGame();
+    document.getElementById("reset").onclick = newGame;
+}
+function cellClick(event) {
+    var event = event,
+        el = event.srcElement || event.target,
+        i = el.id.charAt(0),
         j = el.id.charAt(2);
-
     if ((i == ei && Math.abs(j - ej) == 1) || (j == ej && Math.abs(i - ei) == 1)) {
         document.getElementById(ei + " " + ej).innerHTML = el.innerHTML;
+        el.innerHTML = "";
+        ei = i;
+        ej = j;
+        let q = true;
+        for (let i = 0; i < 4; ++i)
+            for (let j = 0; j < 4; ++j)
+                if (i + j != 6 && document.getElementById(i + " " + j).innerHTML != i * 4 + j + 1) {
+                    q = false;
+                    break;
+                }
+        if (q) alert("Victory!");
     }
-    el.innerHTML = "";
-    ei = i;
-    ej = j;
-
-    let q = true;
-    for (i = 0; i < 4; ++i) {
-        for (j = 0; j < 4; ++j) {
-            if (i + j != 6 && document.getElementById(i + " " + j).innerHTML != i * 4 + j + 1) {
-                q = false;
-                break;
+}
+function newGame() {
+    for (let i = 0; i < 4; ++i) {
+        arr[i] = []
+        for (let j = 0; j < 4; ++j) {
+            if (i + j != 6) {
+                arr[i][j] = i * 4 + j + 1;
+            } else {
+                arr[i][j] = "";
             }
-            if (q) alert("Victory!");
         }
     }
+    ei = 3;
+    ej = 3;
+    for (let i = 0; i < 1600; ++i) {
+        switch (Math.round(3 * Math.random())) {
+            case 0: if (ei != 0) swap(arr, ei, ej, --ei, ej); break; // up
+            case 1: if (ej != 3) swap(arr, ei, ej, ei, ++ej); break; // right
+            case 2: if (ei != 3) swap(arr, ei, ej, ++ei, ej); break; // down
+            case 3: if (ej != 0) swap(arr, ei, ej, ei, --ej); // left
+        }
+    }
+    let table = document.createElement("table"),
+        tbody = document.createElement("tbody");
+        box.appendChild( tbody);
+        box.appendChild(table);
 
+   
+    for (let i = 0; i < 4; ++i) {
+        let row = document.createElement("tr");
+        for (let j = 0; j < 4; ++j) {
+            let cell = document.createElement("td");
+            cell.id = i + " " + j;
+            cell.onclick = cellClick;
+            cell.innerHTML = arr[i][j];
+            row.appendChild(cell);
+        }
+        tbody.appendChild(row);
+    }
+    if (box.childNodes.length == 1) 
+    	box.removeChild(box.firstChild);	
+    box.appendChild(table);	
+    
 }
 
